e093a6a25740ead1c576a28feefea5f8bae05cfd
isabellaaddas <isabellaaddas@gmail.com>
2025-10-30 13:38:02 -0400

    class 10/30

--- src/components/Slideshow.jsx
+++ src/components/Slideshow.jsx
@@ -12,11 +12,19 @@ const Slideshow = () => {
         require.context("./../images/slideshow", false, /\.(png|jpe?g|svg$|webp)/)
     );
 
+    const slideForward = () => {
+        setSlideIndex(slideIndex < images.length - 1 ? slideIndex + 1:0);
+    }
+    
+    const slideBackward = () => {
+        setSlideIndex(slideIndex > 0 ? slideIndex - 1:images.length - 1);
+    }
+
     return (
         <section id="slideshow">
             <img src={images[slideIndex]}/>
-            <a className="arrow" id="right-arrow" href="#">&gt;</a>
-            <a className="arrow" id="left-arrow" href="#">&lt;</a>
+            <a className="arrow" id="right-arrow" href="#" onClick={slideForward}>&gt;</a>
+            <a className="arrow" id="left-arrow" href="#" onClick={slideBackward}>&lt;</a>
         </section>
     );
 };
--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,12 +1,55 @@
-import "./../css/Contact.css"
+import "./../css/Contact.css";
+import {useState} from "react";
 
 const Contact = () => {
+  const [result, setResult] = useState("");
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    setResult("Sending....");
+    const formData = new FormData(event.target);
+
+    formData.append("access_key", "4f59db15-e060-4245-857e-8a98c984a6ee");
+
+    const response = await fetch("https://api.web3forms.com/submit", {
+      method: "POST",
+      body: formData
+    });
+
+    const data = await response.json();
+
+    if (data.success) {
+      setResult("Form Submitted Successfully");
+      event.target.reset();
+    } else {
+      console.log("Error", data);
+      setResult(data.message);
+    }
+  };
+
+  return (
+    <main id="contact" className="main-content">
+        <div>
+            <form onSubmit={onSubmit}>
+                <label for="name">Name:</label><br/>
+                <input type="text" name="name" required/><br/><br/>
+
+                <label for="email">Email:</label><br/>
+                <input type="email" name="email" required/><br/><br/>
+
+                <label for="message">Message:</label><br/>
+                <textarea name="message" required></textarea><br/><br/>
+
+                <button type="submit">Submit Form</button>
+
+            </form>
+
+            <br/>
+            <span>{result}</span>
+        </div>
+    </main>
     
-    return (
-        <main id="contact" className="main-content">
-            <h2>Contact</h2>
-        </main>
-    );
+  );
 };
 
 export default Contact;
